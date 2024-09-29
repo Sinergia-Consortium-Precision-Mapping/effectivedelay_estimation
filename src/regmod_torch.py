6ddab060a70015fdb32558fce72cf2cb1263d0a2
Michael 米高 <miki998Chan@gmail.com>
2024-09-29 18:02:17 +0200

    ADD: - hyperparameter explore (delta + alpha) - joint optim (delta / alpha) - hop depth influence and precompute depth matrices

---
+++ src/regmod_torch.py
@@ -0,0 +1,49 @@
+from typing import Callable, Union
+from collections import Counter
+import numpy as np
+import torch
+from scipy import linalg
+import networkx as nx
+from tqdm.notebook import tqdm
+
+
+def apply_alpha_to_design_torch(design_matrix: torch.tensor, alpha: torch.float):
+    """Create a design matrix for the path model with already provided optimal path matrices and alpha parameter
+
+    Parameters
+    ----------
+    design_matrix : torch.tensor
+        path design matrices.
+    alpha : torch.float (+ autograd)
+        parameter for the sub-optimal paths, by default 0.
+
+    Returns
+    -------
+    torch.tensor
+        design matrix of the path model.
+    """
+
+    # IMPLEMENTATION CONSIDERING ALPHA FACTOR SAME FOR ALL
+    # if isinstance(alpha, (float, int)):
+    #     alpha = [1] + [alpha] * n_subopt
+
+    normalize_vect = torch.zeros(design_matrix[0].shape[0])
+    for i, mat in enumerate(design_matrix):
+        if i == 0:
+            normalize_vect += torch.sign(mat.sum(axis=1))
+        else:
+            normalize_vect += torch.sign(mat.sum(axis=1)) * alpha
+
+    # normalize_vect = sum(normalize_vect)
+    normalize_vect[normalize_vect != 0] = 1 / normalize_vect[normalize_vect != 0]
+
+    design_out = torch.zeros_like(design_matrix[0], dtype=torch.float)
+    for i in range(len(design_matrix)):
+        if i == 0:
+            design_out += design_matrix[i]
+        else:
+            design_out += design_matrix[i] * alpha
+    # print(torch.tensor([design_matrix[i] * alpha for i in range(len(design_matrix))]))
+    # design_out = torch.sum(torch.tensor([design_matrix[i] * alpha for i in range(len(design_matrix))]), axis=0)
+    return torch.diag(normalize_vect) @ design_out
+
